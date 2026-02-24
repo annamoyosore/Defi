49d4a3c80d4eef0c1a8c08fddbad0d689432b18a
annamoyosore <annamoyosore@gmail.com>
2026-02-24 23:38:27 +0100

    Update ERC20.js

--- src/abi/ERC20.js
+++ src/abi/ERC20.js
@@ -1,5 +1,8 @@
-"function balanceOf(address owner) view returns (uint256)",
+const ERC20_ABI = [
+  "function balanceOf(address owner) view returns (uint256)",
   "function decimals() view returns (uint8)",
   "function symbol() view returns (string)",
   "function transfer(address to, uint256 amount) returns (bool)"
 ];
+
+export default ERC20_ABI;
