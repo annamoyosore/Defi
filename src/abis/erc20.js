2dadd3fb1518fd7a3de2da17e2b1f6dc66c2b787
annamoyosore <annamoyosore@gmail.com>
2026-02-24 15:50:50 +0100

    Update

---
+++ src/abis/erc20.js
@@ -0,0 +1,12 @@
+export const ERC20_ABI = [
+  {
+    name: "transfer",
+    type: "function",
+    stateMutability: "nonpayable",
+    inputs: [
+      { name: "recipient", type: "address" },
+      { name: "amount", type: "uint256" }
+    ],
+    outputs: [{ type: "bool" }]
+  }
+];
