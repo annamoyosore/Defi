1f195a9c48517ac32e6d7ca939367f614369ca24
annamoyosore <annamoyosore@gmail.com>
2026-02-24 15:45:21 +0100

    Update

---
+++ src/config.js
@@ -0,0 +1,14 @@
+export const REOWN_PROJECT_ID = "YOUR_PROJECT_ID";
+
+export const supportedTokens = [
+  {
+    symbol: "USDT",
+    address: "0xYourTokenAddress",
+    decimals: 6,
+  },
+  {
+    symbol: "USDC",
+    address: "0xYourTokenAddress",
+    decimals: 6,
+  }
+];
